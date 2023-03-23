6b6a4793e3682eb72c2a8b07630ff13f7ec66a06
maximoaparicio <maxcodingg@gmail.com>
2023-03-23 12:45:25 -0300

    add projects and cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,10 @@ import { useTranslation } from "react-i18next";
 function About() {
   const [t, i18n] = useTranslation("global");
 
+  const downloadCv = () => {
+    window.open("/Curriculum-Vitae-CV.pdf");
+  };
+
   return (
     <div className="dark:bg-slate-800 dark:text-slate-200">
       <section
@@ -15,14 +19,13 @@ function About() {
         </h2>
         <p className="mb-5 text-xl font-semibold">{t("about.paragraph")}</p>
         <div className="mt-12">
-          <a
-            href="/assets/cv/Curriculum-Vitae-CV.pdf"
-            download="Curriculum-vitae"
+          <button
+            onClick={downloadCv}
             className="group rounded-2xl px-6 py-3 bg-green-500 font-bold text-lg text-white relative overflow-hidden cursor-pointer"
           >
             {t("about.cvBtn")}
             <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
-          </a>
+          </button>
         </div>
       </section>
     </div>
