2a049296df61f29746e82df98302050300f255ba
maximoaparicio <maxcodingg@gmail.com>
2023-04-15 18:00:52 -0300

    change photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next'
 
-import yo from "../assets/img/yo.webp";
-import Github from "../Icons/Github.jsx";
-import Linkedin from "../Icons/Linkedin";
+import yo from '../assets/img/yo.webp'
+import Github from '../Icons/Github.jsx'
+import Linkedin from '../Icons/Linkedin'
+import maxilindo from '../assets/img/maxilindo.png'
 
 function Home() {
-  const [t, i18n] = useTranslation("global");
+  const [t, i18n] = useTranslation('global')
 
   return (
     <main
@@ -14,20 +15,20 @@ function Home() {
     >
       <div>
         <img
-          className="mb-3 rounded-full shadow-xl"
-          src={yo}
-          alt="yo"
-          width="300"
-          height="300"
+          src={maxilindo}
+          alt="maximodipaparicio"
+          className="rounded-full shadow-xl mt-3"
+          width={300}
+          height={300}
         />
       </div>
       <div className="md:pl-4 text-center md:text-left">
-        <p className="font-light pl-1">{t("home.presentation")}</p>
+        <p className="font-light pl-1">{t('home.presentation')}</p>
         <h1 className="text-6xl font-semibold">
           <span className="text-lime-600">M</span>
           aximo Dip Aparicio
         </h1>
-        <h3 className="text-3xl pl-1">{t("home.subtitle")}</h3>
+        <h3 className="text-3xl pl-1">{t('home.subtitle')}</h3>
         <div className="flex gap-1 pl-1 py-2 justify-center md:justify-start">
           <button>
             <Linkedin />
@@ -38,7 +39,7 @@ function Home() {
         </div>
       </div>
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import global_es from "./translations/es/global.json";
-import global_en from "./translations/en/global.json";
-import { I18nextProvider } from "react-i18next";
-import i18next from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import global_es from './translations/es/global.json'
+import global_en from './translations/en/global.json'
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18next.use(LanguageDetector).init({
   interpolation: { escapeValue: false },
-  lng: "en",
+  lng: 'en',
   resources: {
     es: {
       global: global_es,
@@ -19,12 +19,12 @@ i18next.use(LanguageDetector).init({
       global: global_en,
     },
   },
-});
+})
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
   </React.StrictMode>
-);
+)
